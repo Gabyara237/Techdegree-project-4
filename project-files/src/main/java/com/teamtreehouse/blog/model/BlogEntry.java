52c954697fb3f66db1bb89a53f0d72c9fd474064
Gabriela Araujo <110007206+Gabyara237@users.noreply.github.com>
2024-12-03 16:59:07 -0800

    Add title, content, and date fields to the BlogEntry class. Implement the addComment method in BlogEntry class. Add author, content, and date fields to the Comment class.

--- project-files/src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
+++ project-files/src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -1,8 +1,61 @@
 package com.teamtreehouse.blog.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class BlogEntry {
+
+    private String title;
+    private String content;
+    private Date date;
+    private List<Comment> comments ;
+
+
+    public BlogEntry(  String title, String content,Date date) {
+        this.date = date;
+        this.content = content;
+        this.title = title;
+    }
+
+    // ######################## GETTERS #######################
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    // ######################## SETTERS #######################
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+
     public boolean addComment(Comment comment) {
         // Store these comments!
+        comments.add(comment);
         return false;
     }
 }
--- project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -1,4 +1,34 @@
 package com.teamtreehouse.blog.model;
 
+import java.util.Date;
+
 public class Comment {
+    private String author;
+    private String content;
+    private Date date;
+
+    // ######################## CONSTRUCTOR #######################
+    public Comment(String author, String content, Date date) {
+        this.author = author;
+        this.content = content;
+        this.date = date;
+    }
+
+    // ######################## GETTERS #######################
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+
+
+
 }
+
